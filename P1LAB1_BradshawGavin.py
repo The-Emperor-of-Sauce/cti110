da5bb8e164ef25d1e502ed73ddb988950ce17d6f
The-Emperor-of-Sauce <bradshag6476@student.faytechcc.edu>
2025-02-05 17:13:19 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_BradshawGavin.py
@@ -0,0 +1,9 @@
+#Gavin Bradshaw
+#2/5/2025
+#P1LAB1
+#Using input and print funcitons 
+first_name=input("Type your First Name:")
+last_name=input("Type yuor Last Name:")
+
+
+print("hello,", first_name, last_name + "!", "welcome to CTI_110")
